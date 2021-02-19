f2163b70146b058f51e3becf1faca377737c3ee5
Ian Koide <ian.koide@protonmail.com>
2021-02-18 20:28:49 -0500

    filtering on assets page, search queries, order by and filters

--- trade/asset/routes.py
+++ trade/asset/routes.py
@@ -9,13 +9,49 @@ from trade.asset.utils import findProfitMargin
 
 asset = Blueprint("asset", __name__)
 
-@asset.route("/assets")
+@asset.route("/assets", methods=["GET", "POST"])
 def assets():
   page = request.args.get("page", 1, type=int)
-
   assets = Asset.query.order_by(Asset.name.asc()).paginate(page, per_page=20)
 
   return render_template("asset/assets.html", assets=assets, active_page="assets", title="Assets")
+
+@asset.route("/assets/filter")
+def filterAssets():
+  page = request.args.get("page", 1, type=int)
+  search = request.args.get("search") if request.args.get("search") else ""
+  searchText = "%{}%".format(search).replace(" ", "_").upper()
+  mainFilter = request.args.get("mainFilter") if request.args.get("mainFilter") else ""
+  orderBy = request.args.get("orderBy") if request.args.get("orderBy") else ""
+
+  if mainFilter == "sell":
+    if orderBy == "asc":
+      assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.sellPrice.asc())
+    else:
+      assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.sellPrice.desc())
+
+  elif mainFilter == "buy":
+    if orderBy == "asc":
+      assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.buyPrice.asc())
+    else:
+      assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.buyPrice.desc())
+
+  elif mainFilter == "volume":
+    if orderBy == "asc":
+      assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.calcVolume().asc())
+    else:
+      assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.calcVolume().desc())
+
+  elif mainFilter == "margin":
+    if orderBy == "asc":
+      assets = Asset.query.filter(Asset.buyPrice!=0).filter(Asset.name.like(searchText)).order_by(Asset.calcMargin().asc())
+    else:
+      assets = Asset.query.filter(Asset.buyPrice!=0).filter(Asset.name.like(searchText)).order_by(Asset.calcMargin().desc())
+
+  else:
+    assets = Asset.query.filter(Asset.name.like(searchText)).order_by(Asset.name.asc())
+
+  return render_template("asset/assets.html", assets=assets.paginate(page, per_page=20), active_page="assets", title="Assets")
 
 @asset.route("/asset/<string:asset_name>", methods=["GET", "POST"])
 def specific_asset(asset_name):
