35f7b8bad0cdc69f42771e2f9358186b6bfca254
Ian Koide <ian.koide@protonmail.com>
2021-02-20 13:10:49 -0500

    fixed candle creationerror where 'timeframe' candles were getting added instead of 'candleAmount' candles.

--- trade/asset/utils.py
+++ trade/asset/utils.py
@@ -133,16 +133,21 @@ def genXMinOHLC(priceType, minutes, asset):
     timeframe = references[index]
     candleAmount = int(minutes/references[index])
 
+    print(f"Timeframe: {timeframe}")
+    print(f"Candles: {candleAmount}")
+
     allCandles = Candle.query.filter_by(asset=asset).filter_by(priceType=priceType).filter_by(timeframe=timeframe).order_by(Candle.creationDate.desc())
 
     # Add number of candles to candles list
     candles = []
 
     for i, j in enumerate(allCandles):
-      if i == timeframe:
+      if i == candleAmount:
         break
       else:
         candles.append(j)
+
+    print(candles)
 
     # Get minimum price from candles
     minimum = candles[0].low
