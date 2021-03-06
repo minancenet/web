3f94efdfeef213fc00440f670655a2d8ccb9a67f
Ian Koide <ian.koide@protonmail.com>
2021-03-06 17:30:32 -0500

    initial pytest setup

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from minance import create_app, db
+from minance.models import User
+
+@pytest.fixture(scope="module")
+def test_client():
+  _app = create_app("flask_test.cfg")
+
+  with _app.test_client() as test_client:
+    with _app.app_context():
+      yield test_client
+
+@pytest.fixture(scope="module")
+def new_user():
+  user = User("user001", "user001@minance.net", "goodPassword")
+  
+  return user
---
+++ tests/unit/test_user.py
@@ -0,0 +1,14 @@
+import pytest
+
+from minance import bcrypt
+
+def test_new_user(new_user):
+  """
+  Given a User model
+  WHEN a new User is created
+  THEN check the username, email and hashed_password are defined correctly
+  """
+
+  assert new_user.username == "user001"
+  assert new_user.email == "user001@minance.net"
+  assert new_user.checkPassword("goodPassword") == True
