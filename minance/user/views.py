a4ea7533959b6144e0d69608a3b088b29911f439
Ian Koide <ikoide@Ians-MBP.home>
2021-03-15 23:53:41 -0400

    fixing flake8

--- minance/user/views.py
+++ minance/user/views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, url_for, redirect, flash
 from flask_login import login_required, current_user
 
+from minance import create_app, db
 from minance.models import Item, Order, Asset
 from minance.user.forms import PlaceOrderForm
 
 user = Blueprint("user", __name__)
+
+app = create_app()
 
 @user.route("/account/dashboard", methods=["GET", "POST"])
 @login_required
@@ -12,18 +15,19 @@ def dashboard():
   pOF = PlaceOrderForm()
   if pOF.validate_on_submit():
     if pOF.orderType == "buy":
-      asset = Asset.query.filter_by(name=(pOF.asset.data).replace(" ", "_").capitalize()).first()
-      order = Order(orderer=current_user, method=pOF.orderType.data, minimumTrust=pOF.minimumTrust.data)
+      with app.app_context():
+        asset = Asset.query.filter_by(name=(pOF.asset.data).replace(" ", "_").capitalize()).first()
+        order = Order(orderer=current_user, method=pOF.orderType.data, minimumTrust=pOF.minimumTrust.data)
 
-      item = Item(amount=pOF.amount.data, asset=asset, owner=order.orderer)
-      order.items.append(item)
-      
-      db.session.add(item)
-      db.session.add(order)
+        item = Item(amount=pOF.amount.data, asset=asset, owner=order.orderer)
+        order.items.append(item)
+        
+        db.session.add(item)
+        db.session.add(order)
 
-      db.session.commit()
+        db.session.commit()
 
-      return redirect(url_for("user.dashboard"))
+        return redirect(url_for("user.dashboard"))
 
   return render_template("user/dashboard.html", pOF=pOF, title="Dashboard")
 
