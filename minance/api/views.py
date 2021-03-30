d428bcdfcf3653681f0588a47152e46d3a8d40d2
ikoide <ian.koide@protonmail.com>
2021-03-30 19:02:45 -0400

    API key validation decorator added to minance/api/views.py

--- minance/api/views.py
+++ minance/api/views.py
@@ -1,19 +1,52 @@
 import json
 import pickle
 from datetime import datetime
+from functools import wraps
 
 from flask import (
   Blueprint,
   render_template,
   abort,
-  Response
+  Response,
+  request
 )
 
+from minance import create_app
 from minance.models import Asset, Candle
 
 api = Blueprint("api", __name__)
 
-@api.route("/api/v1/asset/<string:asset_name>/ohlc/<int:minutes>.json")
+app = create_app()
+V = app.config.get("API_VERSION")
+
+def key_required(clearance):
+  def decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+      if not "key" in request.args:
+        return Response(json.dumps({ "success": False, "cause": "Missing API key."}), mimetype="application/json")
+
+      return f(*args, **kwargs)
+
+    return wrapper
+  return decorator
+
+
+def validateClearance(args, clearance=1):
+  if not "key" in args:
+    response = {
+      "success": False,
+      "cause": "Missing API key."
+    }
+
+    return False, response
+  else:
+    key = args["key"]
+    # Check if key has correct permissions
+
+    return True, {}
+
+@api.route(f"/api/v{V}/asset/<string:asset_name>/ohlc/<int:minutes>.json")
 def assetOHLC(asset_name, minutes):
   asset = Asset.query.filter_by(name=asset_name).first()
   if not asset:
@@ -45,7 +78,7 @@ def assetOHLC(asset_name, minutes):
 
   return Response(json.dumps(outerOHLC), mimetype="application/json")
 
-@api.route("/api/v1/asset/<string:asset_name>/price")
+@api.route(f"/api/v{V}/asset/<string:asset_name>/price")
 def assetPrice(asset_name):
   asset = Asset.query.filter_by(name=asset_name).first()
   if not asset:
@@ -68,3 +101,19 @@ def assetPrice(asset_name):
   outerPrices["buy"] = buyPrices
 
   return Response(json.dumps(outerPrices), mimetype="application/json")
+
+@api.route(f"/api/v{V}/orders")
+@key_required(clearance=1)
+def orders():
+  """All orders for Minance."""
+
+  response = {}
+
+  return Response(json.dumps(response), mimetype="application/json")
+
+@api.route(f"/api/v{V}/bot/<int:bot_id>/orders")
+@key_required(clearance=1)
+def botOrders(bot_id):
+  response = {}
+  
+  return Response(json.dumps(response), mimetype="application/json")
