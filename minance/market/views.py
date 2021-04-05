6ff77758fdb7f56afcf909f80fed2660977761cc
ikoide <ian.koide@protonmail.com>
2021-04-05 15:07:31 -0400

    footer resource links and beginning /order page.

--- minance/market/views.py
+++ minance/market/views.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import (
+  Blueprint,
+  render_template,
+  redirect,
+  url_for,
+  flash,
+  abort
+)
 
 from minance import create_app, db
 from minance.models import Order
@@ -10,3 +17,11 @@ def marketplace():
   orders = Order.query.order_by(Order.order_date.desc())
 
   return render_template("market/marketplace.html", orders=orders, title="Marketplace")
+
+@market.route("/marketplace/order/<int:order_id>")
+def order(order_id):
+  order = Order.query.filter_by(id=order_id).first()
+  if not order:
+    abort(404)
+
+  return render_template("market/order.html", title="Order #"+str(order.id))
